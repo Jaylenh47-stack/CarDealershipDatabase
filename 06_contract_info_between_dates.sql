715685f7365df417f485eaf4ca535b9766983497
Jaylenh47-stack <jaylenh47@gmail.com>
2025-11-25 21:05:09 -0500

    finished queries

---
+++ 06_contract_info_between_dates.sql
@@ -0,0 +1,10 @@
+USE cardealership;
+
+SELECT *
+
+FROM `sales contracts`
+
+WHERE dealershipID = 1 AND 
+ContractDate BETWEEN '2024-10-01' AND '2024-10-31'
+
+
---
+++ Update_dealershipID_sales_contracts.sql
@@ -0,0 +1,25 @@
+-- USE cardealership;
+
+-- UPDATE `sales contracts`
+
+-- SET dealershipID = 1
+
+-- WHERE saleContractID = 3 AND  
+
+-- UPDATE `sales contracts`
+
+-- SET dealershipID = 4
+
+-- WHERE saleContractID = 1
+
+-- UPDATE `sales contracts`
+
+-- SET dealershipID = 2
+
+-- WHERE saleContractID = 2 
+
+UPDATE `sales contracts`
+
+SET dealershipID = 5
+
+WHERE saleContractID = 4
